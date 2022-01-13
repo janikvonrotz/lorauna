1ef04b7a87ae0b18ce2d8c984968c26a90859770
Janik von Rotz <login@janikvonrotz.ch>
2022-01-13 18:40:29 +0100

    update uplink

--- api/uplink.js
+++ api/uplink.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   if (req.body.end_device_ids.device_id == 'eui-a8404189f18318de') {
     saunaId = '5bce139792a5f41e7aa6382b'
   }
-  if (req.body.end_device_ids.device_id == 'device2') {
+  if (req.body.end_device_ids.device_id == 'eui-a84041f7b1838d7d') {
     saunaId = '5e5ebf1d9e960b00086f46f0'
   }
 
